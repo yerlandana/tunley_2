dfdf3e8b75d01e455ab4f7502bb38aad8ce3c5e6
Dana <dyerlanova@gmail.com>
2023-03-12 03:42:37 -0500

    data from json

---
+++ lab-tunley/TracksViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  lab-tunley
+//
+//  Created by Charlie Hieger on 12/1/22.
+//
+
+import UIKit
+
+class TracksViewController: UIViewController, UITableViewDataSource {
+
+    var tracks: [Track] = []
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO: Pt 1 - Set tracks property with mock tracks array
+        tracks = Track.mockTracks
+        print(tracks)
+
+        tableView.dataSource = self
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // TODO: Deselect any selected table view rows
+
+        // Get the index path for the current selected table view row (if exists)
+        if let indexPath = tableView.indexPathForSelectedRow {
+
+            // Deslect the row at the corresponding index path
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // TODO: Pt 1 - Pass the selected track to the detail view controller
+
+        // Get the cell that triggered the segue
+        if let cell = sender as? UITableViewCell,
+           // Get the index path of the cell from the table view
+           let indexPath = tableView.indexPath(for: cell),
+           // Get the detail view controller
+           let detailViewController = segue.destination as? DetailViewController {
+
+            // Use the index path to get the associated track
+            let track = tracks[indexPath.row]
+
+            // Set the track on the detail view controller
+            detailViewController.track = track
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // Get a cell with identifier, "TrackCell"
+        // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
+
+        // Get the track that corresponds to the table view row
+        let track = tracks[indexPath.row]
+
+        // Configure the cell with it's associated track
+        cell.configure(with: track)
+
+        // return the cell for display in the table view
+        return cell
+    }
+}
